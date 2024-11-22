a9021a16346e924b7bf7cabdb638527c843eaf45
Danielr-1998 <daniel.riano@utp.edu.co>
2024-11-21 22:28:41 -0500

    Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
-import path from 'path';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  minify: true,
-  webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    return config;
-  },
+  swcMinify: true,
 };
 
 export default nextConfig;
