2acd2c5a3472f3d73596494a12d051cfedb0bae9
Danielr-1998 <daniel.riano@utp.edu.co>
2024-11-21 22:03:35 -0500

    Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
-// next.config.ts
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
